95db0bd84b61590fbbf3b552741277abfcd24cdf
MawceWithAC <242079@student.stbrn.ac.uk>
2025-09-23 22:53:03 +0100

    Tests.html Now Has A Working Sidebar Only took 2 hours...

---
+++ templates/SideBar.js
@@ -0,0 +1,37 @@
+let IsBarOpen = true;
+
+const CloseSideBar = {
+    transform: ["scaleX(1)","scaleX(0)"]
+};
+const test = [
+    { transform: "rotate(0) scale(1)" },
+    { transform: "rotate(360deg) scale(0)" },
+];
+const Forwards = {
+    duration: 300,
+    iterations: 1,
+    direction: "normal"
+};
+const Backwards =  {...Forwards};
+Backwards.direction = "reverse";
+
+
+function SwapBar()
+{
+    const SideBar = document.querySelector(".sidebar");
+    if (IsBarOpen) {
+        SideBar.animate(CloseSideBar, Forwards);
+        //SideBar.style.setProperty('--Display', 'None');
+        setTimeout(() => {
+            SideBar.style.setProperty('--Display', 'None');
+        }, Forwards.duration-10);
+        IsBarOpen = false;
+    }
+    else if (!IsBarOpen) {
+        SideBar.style.setProperty('--Display', 'block')
+        SideBar.animate(CloseSideBar, Backwards);
+        //SideBar.style.setProperty('--Display', 'None');
+        IsBarOpen = true;
+    }
+    //console.log("Test")
+}
