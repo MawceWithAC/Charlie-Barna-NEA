cf428c8d8f5359279d0a37f76035ea5bc5e9b610
Charlie Barna <242079@student.stbrn.ac.uk>
2025-11-04 15:43:55 +0000

    Closed sidebar on default when aspect ratio is too small or Width<500px

--- templates/static/Scripts/SideBar.js
+++ templates/static/Scripts/SideBar.js
@@ -3,10 +3,6 @@ let IsBarOpen = true;
 const CloseSideBar = {
     transform: ["scaleX(1)","scaleX(0)"]
 };
-const test = [
-    { transform: "rotate(0) scale(1)" },
-    { transform: "rotate(360deg) scale(0)" },
-];
 const Forwards = {
     duration: 300,
     iterations: 1,
@@ -40,3 +36,23 @@ function SwapBar()
     }
     //console.log("Test")
 }
+function SwapBarInstant()
+{
+    const SideBar = document.querySelector(".sidebar");
+	const Main = document.getElementById("Main");
+    if (IsBarOpen) {
+        SideBar.style.setProperty('--Display', 'None');
+		Main.style.width = "100%"
+        IsBarOpen = false;
+    }
+    else if (!IsBarOpen) {
+        SideBar.style.setProperty('--Display', 'block')
+		Main.style.width = "93%"
+        IsBarOpen = true;
+    }
+    //console.log("Test")
+}
+if (screen.width < 500 || screen.width/screen.height >= 2
+) 
+
+{SwapBarInstant(); console.log(screen.width/screen.height)}
